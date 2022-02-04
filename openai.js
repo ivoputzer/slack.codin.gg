04bd210e21b8ec1b508ec82c08b0e6674982533f
Christian Fei <crifei93@gmail.com>
2022-02-04 12:37:42 +0100

    detect language only when tts is used

--- openai.js
+++ openai.js
@@ -13,9 +13,9 @@ const lang = require('./lang.json')
 setImmediate(function qa(ai, rl) {
   rl.question('> ', async (prompt) => {
     const { data: { choices: [{ text: answer }] } } = await ai.createCompletion('text-davinci-001', { prompt, temperature: 0.25, max_tokens: 480, top_p: 1, frequency_penalty: 0, presence_penalty: 0 })
-    const { languages: [{ code }] } = await cld.detect(answer.trimStart())
     rl.write(`${answer.trimStart()}${EOL}`)
     if (process.argv.includes('--tts')) {
+      const { languages: [{ code }] } = await cld.detect(answer.trimStart())
       say.speak(answer.trimStart(), lang[code], 0.45)
     }
     qa(ai, rl)
